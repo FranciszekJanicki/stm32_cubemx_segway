4867a3d615a5bf213d7253cc506af4248d11df36
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-05-02 20:28:08 +0200

    Whoops, i added dutkiewicz but forgot to commit bolrtukeiwicz

---
+++ app/segway/log_manager/log_manager.cpp
@@ -0,0 +1,72 @@
+#include "log_manager.hpp"
+#include "FreeRTOS.h"
+#include "queue_manager.hpp"
+#include "semphr.h"
+#include "task.h"
+#include "usart.h"
+#include "usbd_cdc_if.h"
+#include <array>
+#include <cstring>
+
+namespace segway {
+
+    namespace {
+
+        void log_task(void*) noexcept
+        {
+            auto event = LogEvent{};
+
+            while (1) {
+                if (xQueueReceive(get_log_queue(), &event, pdMS_TO_TICKS(10))) {
+                    auto msg = reinterpret_cast<std::uint8_t*>(event.data());
+                    auto msg_len = std::strlen(event.data());
+
+                    HAL_UART_Transmit(&huart2, msg, msg_len, 100);
+                    CDC_Transmit_FS(msg, msg_len);
+                }
+            }
+        }
+
+        inline void log_queue_init() noexcept
+        {
+            constexpr auto LOG_QUEUE_ITEM_SIZE = sizeof(LogEvent);
+            constexpr auto LOG_QUEUE_ITEMS = 20UL;
+            constexpr auto LOG_QUEUE_STORAGE_SIZE = LOG_QUEUE_ITEM_SIZE * LOG_QUEUE_ITEMS;
+
+            static auto static_log_queue = StaticQueue_t{};
+            static auto log_queue_storage = std::array<std::uint8_t, LOG_QUEUE_STORAGE_SIZE>{};
+
+            set_log_queue(xQueueCreateStatic(LOG_QUEUE_ITEMS,
+                                             LOG_QUEUE_ITEM_SIZE,
+                                             log_queue_storage.data(),
+                                             &static_log_queue));
+        }
+
+        inline void log_task_init() noexcept
+        {
+            constexpr auto LOG_TASK_PRIORITY = 1UL;
+            constexpr auto LOG_TASK_STACK_DEPTH = 1024UL;
+            constexpr auto LOG_TASK_NAME = "log_task";
+            constexpr auto LOG_TASK_ARG = nullptr;
+
+            static auto static_log_task = StaticTask_t{};
+            static auto log_task_stack = std::array<StackType_t, LOG_TASK_STACK_DEPTH>{};
+
+            xTaskCreateStatic(&log_task,
+                              LOG_TASK_NAME,
+                              log_task_stack.size(),
+                              LOG_TASK_ARG,
+                              LOG_TASK_PRIORITY,
+                              log_task_stack.data(),
+                              &static_log_task);
+        }
+
+    }; // namespace
+
+    void log_manager_init() noexcept
+    {
+        log_task_init();
+        log_queue_init();
+    }
+
+}; // namespace segway
